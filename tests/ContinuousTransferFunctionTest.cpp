577050e3384df51f59c6b82f84c9078f0eea6d61
Connor Herron <cwh@vt.edu>
2025-02-10 08:30:50 -0500

    Added CascadeButterworthAndNotchTest() and CascadeOrderShouldntMatter()

--- tests/ContinuousTransferFunctionTest.cpp
+++ tests/ContinuousTransferFunctionTest.cpp
@@ -2,6 +2,7 @@
 #include <Eigen/Dense>
 #include "ContinuousTransferFunction.h"
 #include <memory>
+#include <math.h>
  
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -158,7 +159,52 @@ TEST(ContinuousTransferFunctionTest, CascadingTFsTest) {
   This test cascades a 2nd order Butterworth and Notch TFs.
 */
 TEST(ContinuousTransferFunctionTest, CascadeButterworthAndNotchFilterTest) {
-  ASSERT_TRUE(1.0 == 1.0);
+  
+  //Build Butterworth and Notch filters.
+  std::string name_butter = "Second Order Low-pass Butterworth Filter";
+  double k_butter = 1.0;          // DC Gain
+  double wc = 2*M_PI*10.0;        // Cut-off Frequency
+  VectorXd numerator_butter{1};
+  VectorXd denominator_butter{3};
+  numerator_butter.setZero();
+  denominator_butter.setZero();
+  numerator_butter << wc * wc;
+  denominator_butter << 1.0, sqrt(2) * wc, wc * wc;
+  ContinuousTransferFunction tf_butter{name_butter, k_butter, numerator_butter, denominator_butter};
+  std::cout << "Transfer function " << name_butter << " constructed." << std::endl;
+
+  std::string name_notch = "Second Order Notch Filter";
+  double k_notch = 1.0;         // DC Gain
+  double wn = 60 * 2 * M_PI;    // Attenuate Frequency
+  double Q = 5.0;               // Filter Quality
+  VectorXd numerator_notch{3};
+  VectorXd denominator_notch{3};
+  numerator_notch.setZero();
+  denominator_notch.setZero();
+  numerator_notch << 1.0, 0.0, wn * wn;
+  denominator_notch << 1.0, wn / Q, wn * wn;
+  ContinuousTransferFunction tf_notch{name_notch, k_notch, numerator_notch, denominator_notch};
+  std::cout << "Transfer function " << name_notch << " constructed." << std::endl;
+
+  // Create Combined Tranfer Function.
+  std::string name_combined = "combinedTF";
+  ContinuousTransferFunction severalContinuousTransferFunctions[2] = {tf_butter, tf_notch};
+  ContinuousTransferFunction combinedTF{name_combined, severalContinuousTransferFunctions, 2};
+  std::cout << "Transfer function " << name_combined << " constructed." << std::endl;
+
+  // Create expected output and compare gain, numerator, and denominator of Combined TF to expected output.
+  double k_expected = k_butter * k_notch;
+  VectorXd numerator_expected{3};
+  VectorXd denominator_expected{5}; 
+  numerator_expected.setZero();
+  denominator_expected.setZero();
+  numerator_expected << 3.947841760435743e+03, 0, 5.610763643558538e+08;
+  denominator_expected << 1, 1.642558824493224e+02, 1.527698547677758e+05, 1.292631539212299e+07, 5.610763643558538e+08;
+
+  // Set assertions so that the expected values from MATLAB are what we get in return.
+  EXPECT_NEAR(k_expected, combinedTF.getGain(), tolerance);
+  EXPECT_TRUE(numerator_expected.isApprox(combinedTF.getNumerator(), tolerance));
+  EXPECT_TRUE(denominator_expected.isApprox(combinedTF.getDenominator(), tolerance));
 }
 
 /*
@@ -166,5 +212,46 @@ TEST(ContinuousTransferFunctionTest, CascadeButterworthAndNotchFilterTest) {
   same TF.
 */
 TEST(ContinuousTransferFunctionTest, CascadeOrderShouldntMatterTest) {
-  ASSERT_TRUE(1.0 == 1.0);
+    //Build Butterworth and Notch filters.
+    std::string name_butter = "Second Order Low-pass Butterworth Filter";
+    double k_butter = 1.0;          // DC Gain
+    double wc = 2*M_PI*10.0;        // Cut-off Frequency
+    VectorXd numerator_butter{1};
+    VectorXd denominator_butter{3};
+    numerator_butter.setZero();
+    denominator_butter.setZero();
+    numerator_butter << wc * wc;
+    denominator_butter << 1.0, sqrt(2) * wc, wc * wc;
+    ContinuousTransferFunction tf_butter{name_butter, k_butter, numerator_butter, denominator_butter};
+    std::cout << "Transfer function " << name_butter << " constructed." << std::endl;
+  
+    std::string name_notch = "Second Order Notch Filter";
+    double k_notch = 1.0;         // DC Gain
+    double wn = 60 * 2 * M_PI;    // Attenuate Frequency
+    double Q = 5.0;               // Filter Quality
+    VectorXd numerator_notch{3};
+    VectorXd denominator_notch{3};
+    numerator_notch.setZero();
+    denominator_notch.setZero();
+    numerator_notch << 1.0, 0.0, wn * wn;
+    denominator_notch << 1.0, wn / Q, wn * wn;
+    ContinuousTransferFunction tf_notch{name_notch, k_notch, numerator_notch, denominator_notch};
+    std::cout << "Transfer function " << name_notch << " constructed." << std::endl;
+  
+    // Create Combined Tranfer Functions.
+    std::string name_combined_butter_notch = "Combined TF Butterworth and Notch";
+    ContinuousTransferFunction severalContinuousTransferFunctions_butter_notch[2] = {tf_butter, tf_notch};
+    ContinuousTransferFunction combinedTF_butter_notch{name_combined_butter_notch, severalContinuousTransferFunctions_butter_notch, 2};
+    std::cout << "Transfer function " << name_combined_butter_notch << " constructed." << std::endl;
+
+    std::string name_combined_notch_butter = "Combined TF Notch and Butter";
+    ContinuousTransferFunction severalContinuousTransferFunctions_notch_butter[2] = {tf_notch, tf_butter};
+    ContinuousTransferFunction combinedTF_notch_butter{name_combined_notch_butter, severalContinuousTransferFunctions_notch_butter, 2};
+    std::cout << "Transfer function " << name_combined_notch_butter << " constructed." << std::endl;
+
+  
+    // Set assertions so that the expected values from MATLAB are what we get in return.
+    EXPECT_NEAR(combinedTF_butter_notch.getGain(), combinedTF_notch_butter.getGain(), tolerance);
+    EXPECT_TRUE(combinedTF_butter_notch.getNumerator().isApprox(combinedTF_notch_butter.getNumerator(), tolerance));
+    EXPECT_TRUE(combinedTF_butter_notch.getDenominator().isApprox(combinedTF_notch_butter.getDenominator(), tolerance));
 }
